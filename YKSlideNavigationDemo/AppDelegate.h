a01d55d0d0c2f956517c0b6ba81f2fb48999744f
Yoshiki Kurihara <clouder@gmail.com>
2012-11-19 13:39:43 +0900

    Initial Commit

---
+++ YKSlideNavigationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YKSlideNavigationDemo
+//
+//  Created by Yoshiki Kurihara on 12/11/19.
+//  Copyright (c) 2012年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YKSlideNavigationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YKSlideNavigationDemo
+//
+//  Created by Yoshiki Kurihara on 12/11/19.
+//  Copyright (c) 2012年 Yoshiki Kurihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
